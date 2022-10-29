ef44137a66ed947a3b176d13921b7053ca2ebb3f
obafemitope50 <obafemitope50@gmail.com>
2022-10-28 23:48:25 -0700

    models.py in the musicapp has been updated

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    date_released = models.DateField()
+    likes = models.PositiveIntegerField(default=0)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Lyrics for {self.song_id.title}'
